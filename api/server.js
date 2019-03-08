609ff740d1bc67f4cb8e6775bc77a431945e18ce
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-08 12:45:25 -0500

    Created test for POST /games to insert game. If successful, return 201 status, JSON, and count of insertions.

--- api/server.js
+++ api/server.js
@@ -12,4 +12,16 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: "Server working" });
 });
 
+server.post('/games', async (req, res) => {
+    let { title, genre, releaseYear } = req.body;
+    const game = { title, genre, releaseYear }
+    try {
+        const count = await db('games').insert(game);
+        res.status(201).json({ count });
+
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
 module.exports = server;
--- api/server.spec.js
+++ api/server.spec.js
@@ -9,8 +9,14 @@ describe('server.js', () => {
         expect(process.env.DB_ENV).toBe('testing');
     });
 
-    describe('GET /', async () => {
-        const res = await request(server).get('/');
+    describe('GET /', () => {
+
+        let res;
+
+        beforeEach(async () => {
+            res = await request(server).get('/')
+            return res;
+        })
 
         it('If successful, returns status 200', async () => {
             expect(res.status).toBe(200);
@@ -20,8 +26,38 @@ describe('server.js', () => {
             expect(res.type).toBe('application/json');
         });
 
-        it('If successful, returns JSON', async () => {
+        it('If successful, returns correct message', async () => {
             expect(res.body).toEqual({ message: "Server working" });
+        });
+
+    });
+
+    describe('POST /games', () => {
+
+        const game = {
+            title: 'Pacman',
+            genre: 'Arcade',
+            releaseYear: 1980
+        }
+
+        let res;
+
+        beforeEach(async () => {
+            await db('games').truncate();
+            res = await request(server).post('/games').send(game)
+            return res;
+        })
+
+        it('If successful, returns status 201', async () => {
+            expect(res.status).toBe(201);
+        });
+
+        it('If successful, returns JSON', async () => {
+            expect(res.type).toBe('application/json');
+        });
+
+        it('If successful, inserts game into db and returns added game', async () => {
+            expect(res.body.count).toEqual([1]);
         });
 
     });
